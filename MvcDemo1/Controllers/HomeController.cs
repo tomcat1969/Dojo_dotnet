1f6f8e2a13798426847ee357f4d90a115e39f509
huanglin <tomcat1969@gmail.com>
2020-02-09 12:13:46 -0800

    down with view model

---
+++ MvcDemo1/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MvcDemo1.Models;
+
+
+namespace MvcDemo1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+         public IActionResult Names()
+            {
+                // to a View that has defined a model as @model string[]
+                string[] names = new string[]
+                {
+                "Sally", "Billy", "Joey", "Moose"
+                };
+                return View(names);
+            }
+
+         public IActionResult Message()
+         {
+             string msg = "this is a message !!";
+             //ViewBag.msg = msg;
+             return View(msg);
+         }    
+
+        public IActionResult Numbers()
+        {
+            int[] numbers = new int[]{1,2,3,4,5,6};
+            return View(numbers);
+        }
+
+        public IActionResult Users()
+        {
+            List<User> users = new List<User>(){
+                new User{
+                    FirstName = "lin",
+                    LastName = "Huang"
+                },
+                new User{
+                    FirstName = "lin2",
+                    LastName = "Huang"
+                },
+                new User{
+                    FirstName = "lin3",
+                    LastName = "Huang"
+                }
+            };
+
+            return View(users);
+
+        }
+
+
+        public IActionResult User()
+        {
+            User user = new User(){
+                FirstName = "Marry",
+                LastName = "Wood"
+            };
+            return View(user);
+
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MvcDemo1/Models/User.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace MvcDemo1.Models
+{
+    public class User
+    {
+        public string FirstName {get;set;}
+        public string LastName {get;set;}
+    }
+}
