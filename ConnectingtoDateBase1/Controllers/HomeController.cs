d120a7a9abca87d40b497e6f6da76adae0a70115
huanglin <tomcat1969@gmail.com>
2020-02-11 12:48:43 -0800

    done with random passcode

---
+++ ConnectingtoDateBase1/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ConnectingtoDateBase1.Models;
+using DbConnection;
+
+namespace ConnectingtoDateBase1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Dictionary<string, object>> AllUsers = DbConnector.Query("SELECT * FROM users");
+            // To provide this data, we could use ViewBag or a View Model.  ViewBag shown here:
+            ViewBag.Users = AllUsers;
+            return View();
+            
+        }
+
+        // [HttpGet]
+        // [Route("{userId}")]
+        // public IActionResult Show(int userId)
+        // {
+        //     // One user will be represented as an item in the list of dictionaries, shown here by indexing 0
+        //     Dictionary<string, object> User = DbConnector.Query($"SELECT * FROM users WHERE id = {userId}")[0];
+        //     // Other code
+        // }
+        // // Create a User
+        // [HttpPost]
+        // [Route("create")]
+        // public IActionResult Create(User user)
+        // {
+        //     // other code
+        //     string query = $"INSERT INTO users (FirstName, LastName) VALUES ('{user.FirstName}', '{user.LastName}')";
+        //     DbConnector.Execute(query);
+        //     // other code
+        // }
+
+
+
+        /////////////////////////////////////////////////////////////////
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RandomPasscode/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RandomPasscode.Models;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+
+namespace RandomPasscode.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private string getOnePasscode()
+        {
+            Random random = new Random();
+            char[] array = new char[14];
+            int count = 0;
+            while (count < 14)
+            {
+                int temp = random.Next(48,90);
+                if(temp < 58 || temp > 64) {
+                    array[count] = (char)temp;
+                    count++;
+                }
+            }
+            StringBuilder builder = new StringBuilder();
+            foreach (char val in array){
+                builder.Append(val);
+            }
+            string result = builder.ToString();
+
+            
+
+            return result;
+        }
+        // [HttpGet("")]
+        public IActionResult Index()
+        {
+            int? count = HttpContext.Session.GetInt32("count");
+            if (count == null) {
+                HttpContext.Session.SetInt32("count", 0);
+            }
+            
+            ViewBag.Passcode = getOnePasscode();
+            ViewBag.count = count;
+
+           
+            return View();
+        }
+
+        [HttpGet("generate")]
+        public IActionResult Generate()
+        {
+            System.Console.WriteLine("in generate route **************************");
+            int? count = HttpContext.Session.GetInt32("count");
+            if (count == null) {
+                HttpContext.Session.SetInt32("count", 0);
+            }else {
+                count++;
+                HttpContext.Session.SetInt32("count",(int)count);
+            }
+            string passcode = getOnePasscode();
+            return RedirectToAction("Index");
+        }
+        [HttpGet("test")]
+        public IActionResult Test()
+        {
+            System.Console.WriteLine("in test route ****************");
+            return RedirectToAction("Index");
+        }
+        public IActionResult Privacy()
+        {
+             
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
